4a8313f7c895eb71b271305cbd5077272f2be66e
SAHIL BANSAL <72665907+sahilbansal123-lang@users.noreply.github.com>
2023-02-15 22:34:00 +0530

    Add files via upload

---
+++ backend/dist/static/index-d647773e-8b42881b.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-9e58f0f1-65870047.js";import"./desk-257f76bb-dfcb736e.js";import"./index-087c27b4.js";export{m as DocumentPaneProvider,a as default};
---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'bjaeev50',
+    dataset: 'production'
+  }
+})
